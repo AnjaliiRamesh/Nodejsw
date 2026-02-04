626c6c543fb023d3a1eb262964e53cf725ceb5e2
Anjali <anjaliramesh14012005@gmail.com>
2026-02-04 16:54:12 +0530

    deloitte qs 3

---
+++ three.java
@@ -0,0 +1,14 @@
+public class three {
+    public static void main(String[] args) {
+        String str1 = "12345";
+        String str2 = "01011";
+
+        String result = "";
+        for (int i = 0; i < str1.length(); i++) {   
+            if(str2.charAt(i) == '1') {
+                result += str1.charAt(i);
+            }
+     }
+     System.out.println("Result: " + result);
+}
+}
