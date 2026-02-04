f6483e11a5ad2ab601ad82802f6498bb31c56ebc
Anjali <anjaliramesh14012005@gmail.com>
2026-02-04 16:46:50 +0530

    deloitte qs

---
+++ two.java
@@ -0,0 +1,11 @@
+public class two {
+    public static void main(String[] args) {
+        String input = "enjoy coding";
+
+        // String output = "do not " + input;
+        // System.out.println(output);
+        System.out.println("do not " + input);
+
+    }
+    
+}
