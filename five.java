ac0dfd816785aafc2e86428d9b22df27fc92ea08
Anjali <anjaliramesh14012005@gmail.com>
2026-02-04 17:10:29 +0530

    deloitte 5

---
+++ five.java
@@ -0,0 +1,16 @@
+public class five {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int num : arr) {
+            if(num % 2 == 0) {
+                evenSum += num;
+            } else {
+                oddSum += num;
+            }
+        }
+        System.out.println("Even Sum: " + evenSum);
+        System.out.println("Odd Sum: " + oddSum);
+    }
+}
